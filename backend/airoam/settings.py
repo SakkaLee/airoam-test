5f16f9ad6e51b0ee1a8cb42042f4e7ebcb9baf98
leolee989 <128895689+SakkaLee@users.noreply.github.com>
2025-07-25 10:59:43 +0800

    fix: define INSTALLED_APPS and MIDDLEWARE in settings.py

--- backend/airoam/settings.py
+++ backend/airoam/settings.py
@@ -1,13 +1,30 @@
 import os
-INSTALLED_APPS += [
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
 ]
+
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
-] + MIDDLEWARE
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://airoam.net",
 ]
+
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '') 
